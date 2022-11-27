c06c6340f6eaf56e0b33d860873b875486966315
Fayaz <fayax555@gmail.com>
2022-11-27 20:02:29 +0500

    add hero

---
+++ components/Hero.tsx
@@ -0,0 +1,25 @@
+export default function Hero() {
+  return (
+    <div className='hero mx-auto grid items-center px-4'>
+      <div className=' mx-auto grid w-full max-w-[1200px] gap-10 px-8'>
+        <h1 className=' text-left font-sans text-7xl '>
+          WELCOME TO <span className='block'>THE KINDERGARTEN!</span>
+        </h1>
+        <div className='flex gap-6'>
+          <a
+            href='#'
+            className='rounded-md bg-slate-200 px-4 py-3 text-lg font-bold'
+          >
+            SEE WAITING LISTS
+          </a>
+          <a
+            href='#'
+            className='rounded-md bg-slate-200 px-4 py-3 text-lg font-bold'
+          >
+            PROGRAM & PRICES
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from '@next/font/local'
+
+const neue = localFont({
+  src: '../components/neue.woff2',
+  variable: '--font-neue',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${neue.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
