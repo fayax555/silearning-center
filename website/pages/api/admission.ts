30d987b2959e4f780e2580edbecef1131b82d8a6
Fayaz <fayax555@gmail.com>
2022-12-16 18:38:46 +0000

    update

--- website/pages/api/admission.ts
+++ website/pages/api/admission.ts
@@ -32,21 +32,21 @@ export default async function handler(
     subject: 'Admission Form Received', // Subject line
     html: `
           <div style='font-family: Lucida Sans Unicode;'>
-            <h1 style='font-size: 1.75rem; font-weight: bold; margin:0; padding:0;'>Admission Form</h1>
-            <ul style='padding:0; margin:0; font-size: 1rem; margin-top:0.75rem;'>
-              <li style= 'display: flex; margin:0; margin-top:0.25rem'>
+            <h1 style='font-size: 1.25rem; font-weight: bold; margin:0; padding:0;'>Admission Form Details</h1>
+            <ul style='padding:0; margin:0; font-size: 1rem; margin-top:1rem;'>
+              <li style= 'display: flex; margin:0; margin-top:0.4rem'>
                 <span style= 'width: 150px; font-weight: 600; color: #434446;'>Student Name</span>
                 <span>${studentName}</span>
               </li>
-              <li style= 'display: flex; margin:0; margin-top:0.25rem'>
+              <li style= 'display: flex; margin:0; margin-top:0.4rem'>
                 <span style= 'width: 150px; font-weight: 700; color: #434446;'>Parent Name</span>
                 <span>${parentName}</span>
               </li>
-              <li style= 'display: flex; margin:0; margin-top:0.25rem'>
+              <li style= 'display: flex; margin:0; margin-top:0.4rem'>
                 <span style= 'width: 150px; font-weight: bold; color: #434446;'>Mobile Number</span>
                 <span>${mobile}</span>
               </li>
-              <li style= 'display: flex; margin:0; margin-top:0.25rem'>
+              <li style= 'display: flex; margin:0; margin-top:0.4rem'>
                 <span style= 'width: 150px; font-weight: bold; color: #434446;'>Program</span>
                 <span>${program}</span>
               </li>
