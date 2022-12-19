120fb67f79807a56bdd838d012bec1e990f02f4d
Fayaz <fayax555@gmail.com>
2022-12-19 15:42:00 +0500

    use floating label inputs on contact page

--- website/pages/contact.tsx
+++ website/pages/contact.tsx
@@ -19,14 +19,9 @@ export default function ContactPage() {
 
     try {
       if (mobile.length !== 7) {
-        setError({
-          id: 'mobile',
-          text: 'Mobile number must be 7 digits',
-        })
+        setError({ id: 'mobile', text: 'Mobile number must be 7 digits' })
 
-        return setTimeout(() => {
-          setError({ id: '', text: '' })
-        }, 3000)
+        return setTimeout(() => setError({ id: '', text: '' }), 3000)
       }
 
       setSubmitting(true)
@@ -61,47 +56,59 @@ export default function ContactPage() {
         >
           {formInputs.map(([label, id, type, val, setVal]) => (
             <Fragment key={label}>
-              <div className='flex items-center gap-5'>
-                <label htmlFor={id}>{label}</label>
+              <div className='relative mb-4'>
                 {id === error.id && (
                   <p className='text-sm text-red-700'>{error.text}</p>
                 )}
-              </div>
-              <input
-                required
-                className='mt-1 mb-6 w-full rounded-md border-2 bg-slate-200 px-4 py-2 outline-violet-600'
-                type={type}
-                placeholder={label}
-                id={id}
-                value={val}
-                onChange={(e) => {
-                  if (id === 'mobile') {
-                    if (e.target.value.length > 7) {
-                      setError({
-                        id: 'mobile',
-                        text: 'Mobile number cannot be more than 7 digits',
-                      })
+                <input
+                  className='peer block w-full appearance-none rounded-lg bg-slate-200 px-2.5 pb-2.5 pt-4 text-slate-900 outline-violet-600'
+                  placeholder=' '
+                  required
+                  type={type}
+                  id={id}
+                  value={val}
+                  onChange={(e) => {
+                    if (id === 'mobile') {
+                      if (e.target.value.length > 7) {
+                        setError({
+                          id: 'mobile',
+                          text: 'Mobile number cannot be more than 7 digits',
+                        })
 
-                      return setTimeout(() => {
-                        setError({ id: '', text: '' })
-                      }, 3000)
+                        return setTimeout(() => {
+                          setError({ id: '', text: '' })
+                        }, 3000)
+                      }
                     }
-                  }
 
-                  setVal(e.target.value)
-                }}
-              />
+                    setVal(e.target.value)
+                  }}
+                />
+                <label
+                  htmlFor={id}
+                  className='absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75  cursor-text rounded-md bg-slate-50 px-2 text-slate-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:bg-slate-200  peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:bg-slate-50 peer-focus:px-2  peer-focus:text-violet-900'
+                >
+                  {label}
+                </label>
+              </div>
             </Fragment>
           ))}
 
-          <label htmlFor='message'>Message</label>
+          <div className='relative'>
           <textarea
-            required
-            id='message'
-            placeholder='Message'
-            onChange={(e) => setMessage(e.target.value)}
-            className='mt-1 mb-6 min-h-[120px] w-full rounded-md bg-slate-200 px-4 py-2 outline-violet-600'
-          />
+              required
+              id='message'
+              placeholder=' '
+              onChange={(e) => setMessage(e.target.value)}
+              className='peer block w-full appearance-none rounded-lg bg-slate-200 px-2.5 pb-2.5 pt-4 text-slate-900 outline-violet-600'
+            />
+            <label
+              htmlFor='message'
+              className='absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text rounded-md bg-slate-50 px-2 text-slate-500 duration-300 peer-placeholder-shown:top-6 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:bg-slate-200  peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:bg-slate-50 peer-focus:px-2  peer-focus:text-violet-900'
+            >
+              Message
+            </label>
+          </div>
 
           <button
             disabled={submitting}
