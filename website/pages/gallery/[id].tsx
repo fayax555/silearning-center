e83a12b30159deb4b944ac9d303a4b13a171caeb
Fayaz <fayax555@gmail.com>
2022-12-25 10:51:10 +0500

    .

--- website/pages/gallery/[id].tsx
+++ website/pages/gallery/[id].tsx
@@ -15,8 +15,8 @@ export default function GalleryPage({ gallery }: GalleryPageProps) {
             key={image}
             src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image}`}
             alt=''
-            height={1980}
-            width={1080}
+            width={1980}
+            height={1080}
             className='h-auto w-full'
           />
         ))}
