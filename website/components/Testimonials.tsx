1836b14b59425222eee6029dc94492a0fe01d864
Fayaz <fayax555@gmail.com>
2022-12-28 13:58:20 +0500

    update home testimonial style

--- website/components/Testimonials.tsx
+++ website/components/Testimonials.tsx
@@ -1,21 +1,17 @@
 import { ImQuotesLeft } from 'react-icons/im'
 import { Testimonial } from 'types'
 
-export default function Testimonials({
-  testimonials,
-}: {
-  testimonials: Testimonial[]
-}) {
+export default function Testimonials(p: { testimonials: Testimonial[] }) {
   return (
     <section className='grid py-32 text-center text-slate-800'>
       <div className='mx-auto max-w-[1200px] px-6'>
         <h2 className='text-3xl font-bold md:text-4xl'>WHAT THE PARENTS SAY</h2>
 
         <div className='mt-10 flex max-w-[88vw] gap-5 overflow-x-auto sm:flex-wrap'>
-          {testimonials.map(({ id, name, title, testimonial }) => (
+          {p.testimonials.map(({ id, name, title, testimonial }) => (
             <article
               key={id}
-              className='min-w-[300px] grid-rows-[80%_1fr] flex-1 grid rounded-lg bg-slate-100 p-6'
+              className='grid min-w-[300px] flex-1 grid-rows-[80%_1fr] rounded-lg bg-slate-100 p-6 sm:pb-10 md:pb-9'
             >
               <div>
                 <ImQuotesLeft className='-translate-y-2 text-3xl text-slate-600' />
