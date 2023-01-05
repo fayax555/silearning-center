51cc67fe0c117b3946fe9be216b7c6dc9ad1ce96
Fayaz <fayax555@gmail.com>
2023-01-05 19:29:15 +0500

    delete admin.tsx and cms.tsx to redir from caddy

--- site/pages/admin.tsx
+++
@@ -1,9 +0,0 @@
-export default function Admin() {
-  if (typeof window === 'undefined') return null
-
-  const directusUrl = process.env.NEXT_PUBLIC_DIRECTUS_URL
-  if (process.env.NODE_ENV === 'development' && directusUrl)
-    return (window.location.href = directusUrl + '/admin/login')
-
-  window.location.href = '/cms/admin/login'
-}
